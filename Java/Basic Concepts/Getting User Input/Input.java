05ccd57b318be73b645e12776f0370d51918bc56
Thiago Arruda <thiago_arruda@id.uff.br>
2021-12-28 22:52:18 -0300

    Introduction to Java

---
+++ Java/Basic Concepts/Getting User Input/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println(in.nextLine());
+        
+        in.close();
+    }    
+}
---
+++ Java/Basic Concepts/Variables/Variables.java
@@ -0,0 +1,15 @@
+public class Variables{
+    public static void main(String[] args){
+        String name = "David";
+        int age = 42;
+        double score = 15.9;
+        char group = 'Z';
+        boolean online = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(score);
+        System.out.println(group);
+        System.out.println(online);
+    }
+}
